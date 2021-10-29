ab1672adc1f6075a189fd4af8f09cbdeec587d66
Senna <34717623+senna145@users.noreply.github.com>
2021-10-29 12:56:58 +0200

    Create quote-lijst.js

---
+++ quote-lijst.js
@@ -0,0 +1,26 @@
+let SHEET_ID = '2PACX-1vTzWExNS8fFPsqH1DWS95q6e4hrjfkx2cYkodu0GJNr9ydo4xR-6Sk9GaIzfRXbWgjwyHmUDQVptJlE';
+let API_KEY = process.env.GOOGLE_API_KEY;
+
+function fetchSheet({ spreadsheetId, sheetName, apiKey, complete }) {
+    let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
+    return fetch(url).then(response => 
+        response.json().then(result => {
+            let data = Papa.parse(Papa.unparse(result.values), { header: true });
+            complete(data);
+        })
+    );
+}
+
+function init() {
+    fetchSheet({
+        spreadsheetId: SHEET_ID,
+        sheetName: 'Suggesties Programma Explorers Najaar 2021',
+        apiKey: API_KEY,
+        complete: showInfo
+    });
+}
+
+function showInfo(results) {
+    let data = results.data;
+    console.log(data)
+}
